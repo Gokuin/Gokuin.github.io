b44669bb1132a355cfa3336d5d223a87ac3ec66a
Gokuin <twg12@pitt.edu>
2025-02-17 13:07:33 -0500

    continued working on angular version of home page. Added components and hooked them together with the router. Need to add flesh and css next.

--- Gokuin.Github.Io/src/app/app.routes.ts
+++ Gokuin.Github.Io/src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutPageComponent } from '../about-page/about-page.component';
+import { ProjectsComponent } from '../projects/projects.component';
+import { HobbiesComponent } from '../hobbies/hobbies.component';
+import { LandingPageComponent } from '../landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'about', component:AboutPageComponent},
+    {path: 'projects', component:ProjectsComponent},
+    {path: 'hobbies', component:HobbiesComponent},
+    {path: 'landing', component:LandingPageComponent},
+    {path: '', redirectTo:'/landing', pathMatch: 'full'},
+    { path: '**', redirectTo: '/landing' }
+];
